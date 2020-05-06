e15792f045cc6dedf98c07cd4d6ac281549928a5
Nils Smolinski <nils.smolinski.ext@capgemini.com>
2020-05-06 15:08:54 +0200

    Erzeuge JPEG

--- modules/img.resize.js
+++ modules/img.resize.js
@@ -5,12 +5,13 @@ const resize = (imgPath, width, height) => {
     let [fileName, fileType] = imgPath.split('.');
     console.log(`Starting Resize \t ${colors.cyan(imgPath)} to ${colors.cyan(width)}x${colors.cyan(height)}`);
 
-    let newPath = `${fileName}_${width}_${height}.${fileType}`;
+    let newPath = `${fileName}_${width}_${height}.jpg`;
 
     console.log(`New FileName \t\t ${colors.cyan(fileName)}${colors.green(`_${width}_${height}`)}.${colors.cyan(fileType)}`);
 
     sharp(imgPath)
         .resize(width, height)
+        .jpeg()
         .toFile(newPath, function (err) {
             console.error(err);
         });
